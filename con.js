e27cfc0be0c600e26003d8b70e2136184c0cbd48
GIRIJESHKUSHWAHA <girijeshkushwaha786@gmail.com>
2022-04-09 22:12:27 +0530

    update error message

--- con.js
+++ con.js
@@ -1,7 +1,7 @@
 const mongoose= require("mongoose");
 mongoose.connect('mongodb://localhost:27017/chatSystem',{useNewUrlParser:true, useUnifiedTopology:true })
 .then(()=>console.log("connection is successful...  "))
-.catch(()=>console.log("connection is field...."));
+.catch(()=>console.log("connection is field we can't proceed further...."));
 
 
 //dwdfsfewfesfsfsd
